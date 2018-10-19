5a4c7640c275d34b00d9ee588cc70f1e69cc64c6
Eli Brunette <elibrunette@eagles.ewu.edu>
2018-10-19 16:52:51 -0700

    Finished implementing but not testing

--- Assignment2/BrunetteEHW2/pgmProcess.cu
+++ Assignment2/BrunetteEHW2/pgmProcess.cu
@@ -17,11 +17,48 @@ __device__ float distance( int p1[], int p2[] ) {
 }
 
 //GPU solution for the conpute edge case 
-__device __ void GPUEdge<<<grid, block>>>(d_pixels, maxRow, maxCol,edgeWidth) {
+__device__ void GPUEdge(int * d_pixels, maxRow, maxCol,edgeWidth) {
 
 	rowNum = blockIdx.y * blockDim.y + threadIdx.y;
 	colNum = blockIdx.x * blockDix.x + threadIdx.x;
-	int i = rowNum + colNum;
+	int i = rowNum + colNum
 	
+	if(colNum < maxCol && rowNum < maxRow) {
+		if(colNum >= (maxCol - edgeWidth) || colNum < edgeWidth || rowNum >= (maxRow - edgeWidth) || rowNum < edgeWidth) {
+			d_pixels[i] = 0;
+		}
+	}
+}
+
+//GPU solution for the computed circle case 
+__device__ void GPUCircle(int * d_pixels, int cRow, int cCol, int radius, int maxRow, int maxCol) {
+	row = blockIdx.y * blockDim.y + threadIdx.y;
+	col = blockIdx.x * blockDim.x + threadIdx.x;
+	i = row + col;
+	int p1[2] = {cRow, cCol};
+	int p2[2] = {row, col);
 	
+	if(row < maxRow && col < maxCol) {
+		if(calcDistance(p1, p2) < radius) {
+			d_pixels[i] = 0;
+		}
+	}
+}
+
+//GPU solution for the line
+__device__ void GPULine(int * d_pixels, int maxRow, int maxCol, double slope, double intercept) {
+	row = blockIdx.y * blockDim.y + threadIdx.y;
+	col = blockIdx.x * blockDim.x + threadIdx.x;
+	i = row + col;
+	
+	if(row < maxRow && col < maxCol) {
+		double b = calcIntercept(col, row, slope);
+		if(abs(b - intercept) < .5) {
+			toReturn[i] = 0;
+		}
+	}
+}
+
+__device__ double calcIntercept( int col, int row, slope) {
+	return col - (row * slope);
 }
