97d87e664468014922e8e992c582b9fc138f5f30
Eli Brunette <elibrunette@eagles.ewu.edu>
2018-10-25 16:22:17 -0700

    Initial commit for solving solution
    Created the basic outline for the solution to the problem.

--- Assignment3/kernel1.cu
+++ Assignment3/kernel1.cu
@@ -12,7 +12,17 @@
 __global__ void k1( float* g_dataA, float* g_dataB, int floatpitch, int width) 
 {
     extern __shared__ float s_data[];
-    // TODO, implement this kernel below
-    
+	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+	i = i + 1;
+	
+	int j = blockIdx.y * blockDim.y + threadIdx.y;
+	j = j + 1;
+	
+    if(i < width - 1 || i > 1 || j < width - 1 || j > 1) {
+		//copy memory to shared memory
+		
+		//perform the calculations
+		
+	}
 }
 
