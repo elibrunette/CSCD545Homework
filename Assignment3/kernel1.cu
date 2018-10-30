8e960053a8204ffacf14ed0a598abb604155d63b
Eli Brunette <elibrunette@eagles.ewu.edu>
2018-10-30 11:29:15 -0700

    Compiles before testing
    This is the compiled file before testing

--- Assignment3/kernel1.cu
+++ Assignment3/kernel1.cu
@@ -2,7 +2,7 @@
 #include "kernel1.h"
 
 
-//extern  __shared__  float sdata[];
+extern  __shared__  float sdata[];
 
 ////////////////////////////////////////////////////////////////////////////////
 //! Weighted Jacobi Iteration
@@ -11,10 +11,10 @@
 ////////////////////////////////////////////////////////////////////////////////
 __global__ void k1( float* g_dataA, float* g_dataB, int floatpitch, int width) 
 {
-    extern __shared__ float s_data[4][blockIdx.x];
+        extern __shared__ float s_data[];
 	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
 	i = i + 1;
-	
+	int sharedMaxIndex = blockDim.x + 1;
 	int sharedMemIndex = threadIdx.x;
 
 	int j = blockIdx.y;
@@ -23,41 +23,43 @@ __global__ void k1( float* g_dataA, float* g_dataB, int floatpitch, int width)
     if(i < width - 1 || i > 1 || j < width - 1 || j > 1) {
 		//copy memory to shared memory
 		if(sharedMemIndex == 1) {
-			s_data[1][1] = 0.2f * g_dataA[ i    * pitch +  j   ]; //itself
-			s_data[0][0] = 0.1f * g_dataA[(i-1) * pitch + (j-1)]; //NW
-			s_data[0][1] = 0.1f * g_dataA[(i-1) * pitch +  j   ]; //N
-			s_data[1][0] = 0.1f * g_dataA[ i    * pitch + (j+1)]; //W
-			s_data[2][0] = 0.1f * g_dataA[(i+1) * pitch + (j-1)]; //SW
-			s_data[2][1] = 0.1f * g_dataA[(i+1) * pitch +  j   ]; //S
+			s_data[sharedMaxIndex 	  + 2] = 0.2f * g_dataA[ i    * floatpitch +  j   ]; //itself
+			s_data[			    0] = 0.1f * g_dataA[(i-1) * floatpitch + (j-1)]; //NW
+			s_data[			    1] = 0.1f * g_dataA[(i-1) * floatpitch +  j   ]; //N
+			s_data[sharedMaxIndex 	  + 1] = 0.1f * g_dataA[ i    * floatpitch + (j+1)]; //W
+			s_data[sharedMaxIndex * 2 + 1] = 0.1f * g_dataA[(i+1) * floatpitch + (j-1)]; //SW
+			s_data[sharedMaxIndex * 2 + 2] = 0.1f * g_dataA[(i+1) * floatpitch +  j   ]; //S
 		}
 		else if(sharedMemIndex == width - 2) {
-			s_data[1][width - 2] = 0.2f * g_dataA[ i    * pitch +  j   ]; //itself
-			s_data[0][width - 2] = 0.1f * g_dataA[(i-1) * pitch +  j   ]; //N
-			s_data[0][width - 1] = 0.1f * g_dataA[(i-1) * pitch + (j-1)]; //NE
-			s_data[1][width - 1] = 0.1f * g_dataA[ i    * pitch + (j+1)]; //E
-			s_data[2][width - 1] = 0.1f * g_dataA[(i+1) * pitch + (j+1)]; //SE
-			s_data[2][width - 2] = 0.1f * g_dataA[(i+1) * pitch +  j   ]; //S
+			s_data[2 * sharedMaxIndex - 2	 ] = 0.2f * g_dataA[ i    * floatpitch +  j   ]; //itself
+			s_data[sharedMaxIndex - 2	 ] = 0.1f * g_dataA[(i-1) * floatpitch +  j   ]; //N
+			s_data[sharedMaxIndex - 1	 ] = 0.1f * g_dataA[(i-1) * floatpitch + (j-1)]; //NE
+			s_data[2 * sharedMaxIndex - 1	 ] = 0.1f * g_dataA[ i    * floatpitch + (j+1)]; //E
+			s_data[3 * sharedMaxIndex - 1	 ] = 0.1f * g_dataA[(i+1) * floatpitch + (j+1)]; //SE
+			s_data[3 * sharedMaxIndex - 2	 ] = 0.1f * g_dataA[(i+1) * floatpitch +  j   ]; //S
 		}
 		else {
-			s_data[0][sharedMemIndex] = 0.1f * g_dataA[(i-1) * pitch +  j   ]; //N
-			s_data[1][sharedMemIndex] = 0.2f * g_dataA[ i    * pitch +  j   ]; //itself
-			s_data[2][sharedMemIndex] = 0.1f * g_dataA[(i+1) * pitch +  j   ]; //S
+			s_data[2 * sharedMaxIndex + sharedMemIndex] = 0.1f * g_dataA[(i-1) * floatpitch +  j   ]; //N
+			s_data[2 * sharedMaxIndex + sharedMemIndex] = 0.2f * g_dataA[ i    * floatpitch +  j   ]; //itself
+			s_data[3 * sharedMaxIndex + sharedMemIndex] = 0.1f * g_dataA[(i+1) * floatpitch +  j   ]; //S
 		}
-		__syncthread();
+		__syncthreads();
 		
 		//perform the calculations
-		s_data[3][sharedMemoryIndex] = 	s_data[0][sharedMemoryIndex - 1] +
-						s_data[0][sharedMemoryIndex    ] +
-						s_data[0][sharedMemoryIndex + 1] +
-						s_data[1][sharedMemoryIndex - 1] +
-						s_data[1][sharedMemoryIndex    ] +
-						s_data[1][sharedMemoryIndex + 1] +
-						s_data[2][sharedMemoryIndex - 1] +
-						s_data[2][sharedMemoryIndex    ] +
-						s_data[2][sharedMemoryIndex + 1];
+		s_data[4 * sharedMaxIndex + sharedMemIndex] =	s_data[				sharedMemIndex - 1] + //NW
+								s_data[				sharedMemIndex    ] + //N
+								s_data[				sharedMemIndex + 1] + //NE
+								s_data[sharedMaxIndex + 	sharedMemIndex - 1] + //W
+								s_data[sharedMaxIndex +		sharedMemIndex    ] + //itself
+								s_data[sharedMaxIndex + 	sharedMemIndex + 1] + //E
+								s_data[sharedMaxIndex * 2 + 	sharedMemIndex - 1] + //SW
+								s_data[sharedMaxIndex * 2 + 	sharedMemIndex    ] + //S
+								s_data[sharedMaxIndex * 2 + 	sharedMemIndex + 1];  //SE
 		//copy to shared memory		
+		int midRow = blockIdx.y + 1;
+		int col = blockDim.x * blockIdx.x;
 
-		
+		g_dataB[midRow * width + col] = s_data[3];
 
 	}
 }
